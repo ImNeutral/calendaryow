423802ea1bb6758b0cbcf9428a75061f574d5c9e
stinkymonkeyph <monkeydev.team@gmail.com>
2018-05-10 00:18:30 -0700

    added modal listeners, whitespacing removal

--- calendar/script.js
+++ calendar/script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     var modal           = $('#myModal');
     var span            = $(".close")[0];
+    var confirm         = $("#confirm-button");
+    var cancel          = $("#cancel-button");
 
     $('#calendar').fullCalendar({
         header: {
@@ -40,7 +42,7 @@ $(document).ready(function() {
     
     modal.on("click", function (e) {
         if (e.target != $('.modal-content')[0]) {
-            modal.css("display", "none");
+          //  modal.css("display", "none");
         }
 
     });
@@ -51,5 +53,28 @@ $(document).ready(function() {
     //         console.log("ss");
     //     }
     // }
+
+    //confirm
+
+    replace_whitespaces = function(value){
+            return value.replace(/\s+/, "");
+    }
+
+    confirm.on("click", function(){
+
+        var title = replace_whitespaces($("#title").val());
+        var description = replace_whitespaces($("#description").val());
+
+        if(title != "" && description != ""){
+                alert("non empty proceed with shits");
+        }else{
+                alert("there are some empty shits");
+        }
+
+    });
+
+    cancel.on("click", function(){
+        modal.css("display", "none");
+    });
 
 });
