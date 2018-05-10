10cb3d6eeab8d7fcdb0540491bc24159201c0671
Christian Garillo <christian201316927@gmail.com>
2018-05-09 21:55:11 -0700

    initial GUI

---
+++ calendar/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev',
+            center: 'title',
+            right: 'next'
+        },
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+        ]
+    });
+
+
+    $('.fc-day').hover(function (e) {
+    });
+
+});
